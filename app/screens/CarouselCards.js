b493805ab99e86df20bdaa682ca172018153ca6c
Essien Abasiama <essienabasiama11@gmail.com>
2024-05-23 10:24:05 +0100

    new Fix

--- app/screens/CarouselCards.js
+++ app/screens/CarouselCards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import Carousel, { Pagination } from "react-native-snap-carousel";
 import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from "./CarouselCardItem";
 import data from "./data";
@@ -7,7 +7,10 @@ import data from "./data";
 const CarouselCards = () => {
   const isCarousel = React.useRef(null);
   const [index, setIndex] = React.useState(0);
-
+  const handlePress = () => {
+    // Handle button press action here
+    console.log("Button pressed!");
+  };
   return (
     <View style={styles.container}>
       <Carousel
@@ -22,6 +25,17 @@ const CarouselCards = () => {
         onSnapToItem={(index) => setIndex(index)}
         useScrollView={true}
       />
+
+      {index >= data.length - 1 && (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={handlePress} style={styles.buttonClickMe}>
+            <Text style={styles.buttonText}>Get Started</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handlePress} style={styles.buttonSignIn}>
+            <Text style={styles.buttonTextSignIn}>Click Me</Text>
+          </TouchableOpacity>
+        </View>
+      )}
       {index < data.length - 1 && (
         <Pagination
           dotsLength={data.length}
@@ -43,8 +57,8 @@ const CarouselCards = () => {
             borderColor: "#2D2D5F",
             borderWidth: 2,
           }}
-          inactiveDotOpacity={1}
-          inactiveDotScale={1}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.5}
           tappableDots={true}
         />
       )}
@@ -54,6 +68,52 @@ const CarouselCards = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  buttonClickMe: {
+    width: "100%",
+    height: 55,
+    backgroundColor: "#2D2D5F",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignSelf: "center",
+    justifyContent: "center", // Center content vertically
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: 20, // Add top margin for spacing
+  },
+  buttonSignIn: {
+    width: "100%",
+    height: 55,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignSelf: "center",
+    justifyContent: "center", // Center content vertically
+    alignItems: "center",
+    marginTop: 10,
+    borderWidth: 2,
+    borderColor: "#2D2D5F",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 14,
+    textAlign: "center",
+  },
+  buttonTextSignIn: {
+    color: "#2D2D5F",
+    fontSize: 14,
+    textAlign: "center",
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 50,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
   },
 });
 export default CarouselCards;
--- app/screens/Onboarding.js
+++ app/screens/Onboarding.js
@@ -5,13 +5,4 @@ import CarouselCards from "./CarouselCards";
 const Onboarding = () => {
   return <CarouselCards />;
 };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     padding: 50,
-//   },
-// });
 export default Onboarding;
